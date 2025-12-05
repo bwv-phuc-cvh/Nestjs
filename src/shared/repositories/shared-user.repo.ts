6549cedd81a023d8d981c60b1e145cf45502041a
phuc <phuccvh2001@gmail.com>
2025-12-05 16:50:01 +0700

    feature user

--- src/shared/repositories/shared-user.repo.ts
+++ src/shared/repositories/shared-user.repo.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../services/prisma.service';
+import { UserType } from '../models/user.model';
 
 @Injectable()
 export class SharedUserRepository {
@@ -8,6 +9,33 @@ export class SharedUserRepository {
   async findUnique(uniqueObject: { email: string } | { id: number }) {
     return this.prismaService.user.findUnique({
       where: uniqueObject,
+      include: {
+        role: {
+          select: {
+            id: true,
+            name: true,
+            permissions: {
+              select: {
+                id: true,
+                name: true,
+                module: true,
+                path: true,
+                method: true,
+              },
+              where: {
+                deletedAt: null,
+              },
+            },
+          },
+        },
+      },
+    });
+  }
+
+  async updateUser(userId: number, data: Partial<UserType>) {
+    return this.prismaService.user.update({
+      where: { id: userId },
+      data,
     });
   }
 }
